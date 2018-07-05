de50d1e09f1ca68d5796e260a171c072c3075bdb
Scott Roth <scott.roth@armedia.com>
2018-07-05 14:46:53 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='MLBLiveScoreboard',
+    version='0.02',
+    packages=['venv.Lib.distutils', 'venv.Lib.encodings'],
+    url='',
+    license='',
+    author='sroth',
+    author_email='',
+    description=''
+)
